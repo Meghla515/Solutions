5e54acd05b49b757b741df138cccc44652c7ef30
Meghla <meghla515@gmail.com>
2021-05-22 23:45:42 +0600

    insert a node at the head of a linked list

--- Solutions/Solutions/Program.cs
+++ Solutions/Solutions/Program.cs
@@ -1,12 +1,102 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
 namespace Solutions
 {
     class Program
     {
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+        }
+
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep)
+        {
+            while (node != null)
+            {
+                Console.Write(node.data);
+
+                node = node.next;
+
+                if (node != null)
+                {
+                    Console.Write(sep);
+                }
+            }
+        }
+
+        // Complete the insertNodeAtHead function below.
+
+        /*
+         * For your reference:
+         *
+         * SinglyLinkedListNode {
+         *     int data;
+         *     SinglyLinkedListNode next;
+         * }
+         *
+         */
+        static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data)
+        {
+            var head = new SinglyLinkedListNode(data);
+
+            if (llist != null)
+            {
+                head.next = llist;
+            }
+
+            return head;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                SinglyLinkedListNode llist_head = insertNodeAtHead(llist.head, llistItem);
+                llist.head = llist_head;
+            }
+
+
+
+            PrintSinglyLinkedList(llist.head, "\n");
+            Console.WriteLine();
         }
     }
 }
+
