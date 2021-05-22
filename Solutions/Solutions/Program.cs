2447edb81d27ec8896c1b948142ef58a2f6b0326
Meghla <meghla515@gmail.com>
2021-05-23 04:24:49 +0600

    compare two link list

--- Solutions/Solutions/Program.cs
+++ Solutions/Solutions/Program.cs
@@ -1,102 +1,126 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
 
-namespace Solutions
+class Solution
 {
-    class Program
+
+    class SinglyLinkedListNode
     {
-        class SinglyLinkedListNode
-        {
-            public int data;
-            public SinglyLinkedListNode next;
+        public int data;
+        public SinglyLinkedListNode next;
 
-            public SinglyLinkedListNode(int nodeData)
-            {
-                this.data = nodeData;
-                this.next = null;
-            }
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
         }
 
-        class SinglyLinkedList
+        public void InsertNode(int nodeData)
         {
-            public SinglyLinkedListNode head;
-            public SinglyLinkedListNode tail;
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
 
-            public SinglyLinkedList()
+            if (this.head == null)
             {
-                this.head = null;
-                this.tail = null;
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
             }
 
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the CompareLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static bool CompareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+    {
+        var result = true;
+
+        while (head1 != null && head2 != null)
+        {
+            if (head1.data != head2.data)
+                return false;
+
+            head1 = head1.next;
+            head2 = head2.next;
         }
 
-        static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep)
+        if (head1 != null || head2 != null)
+            return false;
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+
+        int tests = Convert.ToInt32(Console.ReadLine());
+
+        for (int testsItr = 0; testsItr < tests; testsItr++)
         {
-            while (node != null)
+            SinglyLinkedList llist1 = new SinglyLinkedList();
+
+            int llist1Count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llist1Count; i++)
             {
-                Console.Write(node.data);
-
-                node = node.next;
-
-                if (node != null)
-                {
-                    Console.Write(sep);
-                }
-            }
-        }
-
-        // Complete the insertNodeAtHead function below.
-
-        /*
-         * For your reference:
-         *
-         * SinglyLinkedListNode {
-         *     int data;
-         *     SinglyLinkedListNode next;
-         * }
-         *
-         */
-        static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data)
-        {
-            var head = new SinglyLinkedListNode(data);
-
-            if (llist != null)
-            {
-                head.next = llist;
+                int llist1Item = Convert.ToInt32(Console.ReadLine());
+                llist1.InsertNode(llist1Item);
             }
 
-            return head;
-        }
+            SinglyLinkedList llist2 = new SinglyLinkedList();
 
-        static void Main(string[] args)
-        {
-            SinglyLinkedList llist = new SinglyLinkedList();
+            int llist2Count = Convert.ToInt32(Console.ReadLine());
 
-            int llistCount = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < llistCount; i++)
+            for (int i = 0; i < llist2Count; i++)
             {
-                int llistItem = Convert.ToInt32(Console.ReadLine());
-                SinglyLinkedListNode llist_head = insertNodeAtHead(llist.head, llistItem);
-                llist.head = llist_head;
+                int llist2Item = Convert.ToInt32(Console.ReadLine());
+                llist2.InsertNode(llist2Item);
             }
 
+            bool result = CompareLists(llist1.head, llist2.head);
 
-
-            PrintSinglyLinkedList(llist.head, "\n");
-            Console.WriteLine();
+            Console.WriteLine((result ? 1 : 0));
         }
     }
 }
-
